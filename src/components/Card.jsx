de54f027c25f5ac0e6b72b3f62feb1ed50046427
guzziksen96 <dyska96@interia.pl>
2018-07-18 22:56:02 +0200

    handling on click event again doesnt work

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,8 +18,17 @@ class Card extends Component {
         this.setState({ chosenIcon: icon
         });
     }
+    
     render() {
+        const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
 
+        const icons = numbers.map((number) => 
+           <IconTemplate key={number}
+                cords={CardIconCords[number]}   
+                icon ={IconsContainer[number]} 
+                onClick={() => this.handleChange(number)}/>
+        
+        );
         return (
             <div style={style}>
                 {icons} 
@@ -29,7 +38,6 @@ class Card extends Component {
 }
 
 const style = {
-
     width: 500,
     height: 500,
     borderRadius: "50%",
@@ -37,15 +45,5 @@ const style = {
     backgroundColor: "#eee",
     position: "relative"
 }
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
-
-const icons = numbers.map((number) => 
-   <IconTemplate key={number}
-        cords={CardIconCords[number]}   
-        icon ={IconsContainer[number]} />
-
-);
-
 
 export default Card;
